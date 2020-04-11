28235df123c79793c3bd680a508e84df8467d717
Mina UAE <mina.isaac.2000@gmail.com>
2020-04-12 01:50:02 +0200

    Updates

---
+++ precache-manifest.ff6de506b0bcd1488db9587c2922edfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d0e4e9efe80fae2e90b13e423c972f",
+    "url": "/search-heros/index.html"
+  },
+  {
+    "revision": "0df31121a86728d4e1e3",
+    "url": "/search-heros/static/css/main.97f4df62.chunk.css"
+  },
+  {
+    "revision": "e6746f6785afb39aa5ad",
+    "url": "/search-heros/static/js/2.3ae3fdf0.chunk.js"
+  },
+  {
+    "revision": "faf185b8e3f14ff0093219369cfa4de4",
+    "url": "/search-heros/static/js/2.3ae3fdf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df31121a86728d4e1e3",
+    "url": "/search-heros/static/js/main.062353fe.chunk.js"
+  },
+  {
+    "revision": "c932dda8f4d7a0dcc0f3",
+    "url": "/search-heros/static/js/runtime-main.51312de7.js"
+  }
+]);
